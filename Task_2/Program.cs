6418afc76c4c78fa49907d4982b48748616522ac
Egor <effrosty@ya.ru>
2022-08-22 00:02:11 +0300

    Complete 27 task

---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 27
+
+using static System.Console;
+
+Clear();
+
+WriteLine("введите число");
+int i = Convert.ToInt32(ReadLine()); 
+
+int sum = 0;
+
+while (i > 0)
+{
+int num = i % 10;
+i = i / 10;
+sum = sum + num;
+}
+WriteLine("сумма всех цифр в числе равна: " + sum);
