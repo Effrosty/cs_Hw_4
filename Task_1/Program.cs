4a176b9a99262f67e8f9115ebc3ec88c2d405815
Egor <effrosty@ya.ru>
2022-08-21 23:57:44 +0300

    Complete 25 task

---
+++ Task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25
+
+using static System.Console;
+Clear();
+
+
+WriteLine("введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+
+WriteLine("введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int up = a;
+
+for (int i = 1; i < b; i++)
+{
+up = up * a;
+}
+Console.WriteLine("A в степени B равно: " + up);
